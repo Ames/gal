30421ac04a8b1d7e88a3f4658b80c439e8ed4b07
Jonathan Gluck <jonathangluck08854@gmail.com>
2012-05-16 15:27:32 -0400

    meh

--- gal.js
+++ gal.js
@@ -66,9 +66,14 @@ function init(){
     var p = document.createElement('a');
     var pd = document.createElement('div');
     var pt = document.createElement('p');
+    var tc = document.createElement('div);
+    tc.style.height = pd.style.height/2;
+    tc.style.width = pd.style.width;
+    tc.style.opacity = 0;
     p.appendChild(pd);
     pt.innerHTML = "../";
-    pd.appendChild(pt)
+    pd.appendChild(tc);
+    pd.appendChild(pt);
     pd.className='thumb';
     p.href = getParent();
     pd.style.width = 200;
